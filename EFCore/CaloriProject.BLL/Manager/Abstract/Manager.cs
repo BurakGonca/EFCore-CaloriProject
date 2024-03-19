5465267db7b5832a39b585d9a2c48f9242d471ce
Burak Gonca <goncabrk@gmail.com>
2024-03-19 17:34:36 +0300

    BLL

---
+++ EFCore/CaloriProject.BLL/Manager/Abstract/Manager.cs
@@ -0,0 +1,33 @@
+﻿using CaloriProject.DAL.Repostory.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaloriProject.BLL.Manager.Abstract
+{
+    public abstract class Manager<TEntity> where TEntity : class
+    {
+        protected IRepostory<TEntity> _repository;
+
+        public void Add(TEntity entity)
+        {
+            _repository.Add(entity);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _repository.Remove(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _repository.Update(entity);
+        }
+
+
+
+
+    }
+}
---
+++ EFCore/CaloriProject.BLL/Models/KategoriModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaloriProject.BLL.Models
+{
+    public class KategoriModel
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+    }
+}
