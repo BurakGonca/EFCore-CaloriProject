ead314696588b02d5b07591d25f1a167786589fa
mehmetcagri39 <mehmetcagri.celenk@bilgeadamboost.com>
2024-03-22 17:34:46 +0300

    Ayarlar ekranı oluşturuldu, sıkıntı varsa revert

--- EFCore/CaloriProject.UI/Forms/AyarlarSayfasi.Designer.cs
+++ EFCore/CaloriProject.UI/Forms/AyarlarSayfasi.Designer.cs
@@ -29,31 +29,321 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            label1 = new Label();
+            txt_Ad = new TextBox();
+            txt_Soyad = new TextBox();
+            txt_Kilo = new TextBox();
+            txt_Boy = new TextBox();
+            txt_Email = new TextBox();
+            dogumTarihiPicker = new DateTimePicker();
+            label8 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label17 = new Label();
+            label7 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            btn_Kaydet = new Button();
+            btn_Sil = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(497, 446);
+            button1.Location = new Point(328, 501);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(102, 45);
+            button1.Size = new Size(108, 50);
             button1.TabIndex = 0;
-            button1.Text = "ANA SAYFA";
+            button1.Text = "ANA SAYFAYA DÖN";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft YaHei UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(328, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(133, 42);
+            label1.TabIndex = 1;
+            label1.Text = "Ayarlar";
+            // 
+            // txt_Ad
+            // 
+            txt_Ad.BackColor = SystemColors.Window;
+            txt_Ad.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            txt_Ad.Location = new Point(273, 119);
+            txt_Ad.Name = "txt_Ad";
+            txt_Ad.Size = new Size(265, 27);
+            txt_Ad.TabIndex = 46;
+            // 
+            // txt_Soyad
+            // 
+            txt_Soyad.BackColor = SystemColors.Window;
+            txt_Soyad.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            txt_Soyad.Location = new Point(273, 166);
+            txt_Soyad.Name = "txt_Soyad";
+            txt_Soyad.Size = new Size(265, 27);
+            txt_Soyad.TabIndex = 47;
+            // 
+            // txt_Kilo
+            // 
+            txt_Kilo.BackColor = SystemColors.Window;
+            txt_Kilo.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            txt_Kilo.Location = new Point(273, 260);
+            txt_Kilo.Name = "txt_Kilo";
+            txt_Kilo.Size = new Size(265, 27);
+            txt_Kilo.TabIndex = 48;
+            // 
+            // txt_Boy
+            // 
+            txt_Boy.BackColor = SystemColors.Window;
+            txt_Boy.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            txt_Boy.Location = new Point(273, 308);
+            txt_Boy.Name = "txt_Boy";
+            txt_Boy.Size = new Size(265, 27);
+            txt_Boy.TabIndex = 49;
+            // 
+            // txt_Email
+            // 
+            txt_Email.BackColor = SystemColors.Window;
+            txt_Email.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            txt_Email.Location = new Point(273, 358);
+            txt_Email.Name = "txt_Email";
+            txt_Email.Size = new Size(265, 27);
+            txt_Email.TabIndex = 50;
+            // 
+            // dogumTarihiPicker
+            // 
+            dogumTarihiPicker.Location = new Point(273, 220);
+            dogumTarihiPicker.Margin = new Padding(3, 2, 3, 2);
+            dogumTarihiPicker.Name = "dogumTarihiPicker";
+            dogumTarihiPicker.Size = new Size(265, 23);
+            dogumTarihiPicker.TabIndex = 51;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.Transparent;
+            label8.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label8.ForeColor = SystemColors.ControlText;
+            label8.Location = new Point(158, 119);
+            label8.Name = "label8";
+            label8.Size = new Size(28, 19);
+            label8.TabIndex = 52;
+            label8.Text = "Ad";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label2.ForeColor = SystemColors.ControlText;
+            label2.Location = new Point(151, 166);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 19);
+            label2.TabIndex = 53;
+            label2.Text = "Soyad";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label3.ForeColor = SystemColors.ControlText;
+            label3.Location = new Point(151, 220);
+            label3.Name = "label3";
+            label3.Size = new Size(99, 19);
+            label3.TabIndex = 54;
+            label3.Text = "Doğum Tarihi";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label4.ForeColor = SystemColors.ControlText;
+            label4.Location = new Point(151, 260);
+            label4.Name = "label4";
+            label4.Size = new Size(35, 19);
+            label4.TabIndex = 55;
+            label4.Text = "Kilo";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label5.ForeColor = SystemColors.ControlText;
+            label5.Location = new Point(151, 308);
+            label5.Name = "label5";
+            label5.Size = new Size(35, 19);
+            label5.TabIndex = 56;
+            label5.Text = "Boy";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label6.ForeColor = SystemColors.ControlText;
+            label6.Location = new Point(151, 358);
+            label6.Name = "label6";
+            label6.Size = new Size(51, 19);
+            label6.TabIndex = 57;
+            label6.Text = "E-mail";
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.BackColor = Color.Transparent;
+            label17.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label17.Location = new Point(254, 124);
+            label17.Name = "label17";
+            label17.Size = new Size(13, 19);
+            label17.TabIndex = 58;
+            label17.Text = ":";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label7.Location = new Point(254, 171);
+            label7.Name = "label7";
+            label7.Size = new Size(13, 19);
+            label7.TabIndex = 59;
+            label7.Text = ":";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.Transparent;
+            label9.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label9.Location = new Point(254, 220);
+            label9.Name = "label9";
+            label9.Size = new Size(13, 19);
+            label9.TabIndex = 60;
+            label9.Text = ":";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.Transparent;
+            label10.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label10.Location = new Point(254, 265);
+            label10.Name = "label10";
+            label10.Size = new Size(13, 19);
+            label10.TabIndex = 61;
+            label10.Text = ":";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = Color.Transparent;
+            label11.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label11.Location = new Point(254, 308);
+            label11.Name = "label11";
+            label11.Size = new Size(13, 19);
+            label11.TabIndex = 62;
+            label11.Text = ":";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.BackColor = Color.Transparent;
+            label12.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label12.Location = new Point(254, 358);
+            label12.Name = "label12";
+            label12.Size = new Size(13, 19);
+            label12.TabIndex = 63;
+            label12.Text = ":";
+            // 
+            // btn_Kaydet
+            // 
+            btn_Kaydet.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            btn_Kaydet.Location = new Point(273, 408);
+            btn_Kaydet.Name = "btn_Kaydet";
+            btn_Kaydet.Size = new Size(126, 43);
+            btn_Kaydet.TabIndex = 64;
+            btn_Kaydet.Text = "Kaydet";
+            btn_Kaydet.UseVisualStyleBackColor = true;
+            btn_Kaydet.Click += btn_Kaydet_Click;
+            // 
+            // btn_Sil
+            // 
+            btn_Sil.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            btn_Sil.Location = new Point(405, 408);
+            btn_Sil.Name = "btn_Sil";
+            btn_Sil.Size = new Size(133, 43);
+            btn_Sil.TabIndex = 65;
+            btn_Sil.Text = "Hesabı Sil";
+            btn_Sil.UseVisualStyleBackColor = true;
+            btn_Sil.Click += btn_Sil_Click;
+            // 
             // AyarlarSayfasi
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(815, 624);
+            ClientSize = new Size(784, 562);
+            Controls.Add(btn_Sil);
+            Controls.Add(btn_Kaydet);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label7);
+            Controls.Add(label17);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label8);
+            Controls.Add(dogumTarihiPicker);
+            Controls.Add(txt_Email);
+            Controls.Add(txt_Boy);
+            Controls.Add(txt_Kilo);
+            Controls.Add(txt_Soyad);
+            Controls.Add(txt_Ad);
+            Controls.Add(label1);
             Controls.Add(button1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "AyarlarSayfasi";
             Text = "AyarlarSayfasi";
+            Load += AyarlarSayfasi_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private Label label1;
+        private TextBox txt_Ad;
+        private TextBox txt_Soyad;
+        private TextBox txt_Kilo;
+        private TextBox txt_Boy;
+        private TextBox txt_Email;
+        private DateTimePicker dogumTarihiPicker;
+        private Label label8;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label17;
+        private Label label7;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Button btn_Kaydet;
+        private Button btn_Sil;
     }
 }
--- EFCore/CaloriProject.UI/Forms/AyarlarSayfasi.cs
+++ EFCore/CaloriProject.UI/Forms/AyarlarSayfasi.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using CaloriProject.BLL.Manager.Concrete;
+using CaloriProject.BLL.MappingProfile;
+using CaloriProject.BLL.Models;
+using CaloriProject.DAL.Context;
+using CaloriProject.DAL.Entities;
+using CaloriProject.DAL.Repostory.Abstract;
+using CaloriProject.DAL.Repostory.Concrete;
+using Microsoft.VisualBasic.Logging;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,6 +24,9 @@ namespace CaloriProject.UI.Forms
     {
         private AnaSayfa anaSayfa;
 
+        CaloriDBContext db = new CaloriDBContext();
+        Kullanici kullanici = new Kullanici();
+
         public AyarlarSayfasi(AnaSayfa ana)
         {
             Program.AktifSayfa = this;
@@ -27,6 +38,72 @@ namespace CaloriProject.UI.Forms
         {
             Program.AktifSayfa.Hide();
             anaSayfa.Show();
+        }
+
+        private void AyarlarSayfasi_Load(object sender, EventArgs e)
+        {
+            txt_Ad.Text = Program.kullaniciModel.Ad;
+            txt_Soyad.Text = Program.kullaniciModel.Soyad; ;
+            txt_Boy.Text = Program.kullaniciModel.Boy.ToString();
+            txt_Kilo.Text = Program.kullaniciModel.Kilo.ToString();
+            txt_Email.Text = Program.kullaniciModel.EMail;
+            dogumTarihiPicker.Text = Program.kullaniciModel.DogumTarihi.ToShortDateString();
+
+        }
+
+
+
+        private void btn_Kaydet_Click(object sender, EventArgs e)
+        {
+
+            var guncelle = db.Kullanicilar.First(x => x.EMail == Program.kullaniciModel.EMail);
+            guncelle.Ad = txt_Ad.Text;
+            guncelle.Soyad = txt_Soyad.Text;
+            guncelle.Boy = Convert.ToDouble(txt_Boy.Text);
+            guncelle.Kilo = Convert.ToDouble(txt_Kilo.Text);
+            guncelle.DogumTarihi = Convert.ToDateTime(dogumTarihiPicker.Text);
+            guncelle.EMail = txt_Email.Text;
+
+
+
+            // Program.kullaniciModel içindeki verilerin güncellenmesi
+            Program.kullaniciModel.Ad = txt_Ad.Text;
+            Program.kullaniciModel.Soyad = txt_Soyad.Text;
+            Program.kullaniciModel.Boy = Convert.ToDouble(txt_Boy.Text);
+            Program.kullaniciModel.Kilo = Convert.ToDouble(txt_Kilo.Text);
+            //Program.kullaniciModel.EMail = yeniEmail;
+            Program.kullaniciModel.DogumTarihi = Convert.ToDateTime(dogumTarihiPicker.Text);
+
+
+
+            //Veritabanında kullanıcı modelinin güncellenmesi için uygun kodlar buraya yazılmalıdır
+            //Örneğin, veritabanı bağlantısı kurma, güncelleme sorgusu oluşturma ve çalıştırma işlemleri burada gerçekleştirilebilir
+            //Veritabanı işlemleri için uygun kütüphaneleri ve yöntemleri kullanarak veritabanına güncelleme yapılabilir
+
+            DialogResult result = MessageBox.Show("Profil güncellenecek onaylıyor musun?", "Onaylıyorum", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                kullanici.Update(guncelle);
+                MessageBox.Show("Profil başarıyla güncellendi.", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btn_Sil_Click(object sender, EventArgs e)
+        {
+            var sil = db.Kullanicilar.FirstOrDefault(x => x.EMail == Program.kullaniciModel.EMail);
+
+            DialogResult result = MessageBox.Show("Profili silmek istiyor musun?", "Evet", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+            if (result == DialogResult.OK)
+            {
+                kullanici.Delete(sil);
+                MessageBox.Show("Profil başarıyla silindi.", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Giris giris = new Giris();
+                giris.Show();
+                this.Hide();
+            }
         }
     }
 }
